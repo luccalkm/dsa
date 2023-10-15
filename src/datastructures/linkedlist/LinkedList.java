a20ae52500fa43d0ff4549ced2a380aa689ef750
luccalkm <lucca.kleinsorgen@gmail.com>
2023-10-15 17:21:53 -0300

    Queue started

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -1,123 +1,142 @@
 package datastructures.linkedlist;
 
-public class LinkedList <T> {
+public class LinkedList<T> {
 	private Node<T> head;
 	private Node<T> tail;
 	private int elements;
-	
+
 	public LinkedList() {
 		head = null;
 		tail = null;
 		elements = 0;
 	}
-	
+
 	private void isFirstElement(Node<T> firstNode) {
-		if(elements == 0) {
+		if (elements == 0) {
 			head = firstNode;
 			tail = firstNode;
 		}
 	}
-	
+
 	public LinkedList(T value) {
 		Node<T> newNode = new Node<T>(value);
 		head = newNode;
 		tail = newNode;
 		elements = 1;
 	}
-	
+
 	public Node<T> indexOf(Node<T> node) {
-		if(elements == 0) return null;
-		
+		if (elements == 0)
+			return null;
+
 		Node<T> currentNode = head;
-		while(currentNode != null) {
-			if(currentNode.equals(node)) {
+		while (currentNode != null) {
+			if (currentNode.equals(node)) {
 				return currentNode;
 			}
 			currentNode = currentNode.next;
 		}
 		return null;
 	}
-	
+
 	public void set(T value, int index) {
-		if(index < 0 || index >= elements) return;
-		
+		if (index < 0 || index >= elements)
+			return;
+
 		Node<T> temp = this.get(index);
 		temp.value = value;
 	}
-	
+
 	public Node<T> get(int index) {
-		if(index < 0 || index >= elements) return null;
-		
+		if (index < 0 || index >= elements)
+			return null;
+
 		Node<T> currentNode = head;
 		int currentIndex = 0;
-		while(currentIndex < index) {
+		while (currentIndex < index) {
 			currentNode = currentNode.next;
 			currentIndex++;
 		}
-		
+
 		return currentNode;
 	}
-	
-	public void append(Node<T> newNode) {
+
+	public void append(T element) {
+		Node<T> newNode = new Node<>(element);
 		this.isFirstElement(newNode);
-		
+
 		tail.next = newNode;
 		tail = newNode;
 		elements++;
 	}
-	
-	public void prepend(Node<T> newNode) {
+
+	public void prepend(T element) {
+		Node<T> newNode = new Node<>(element);
 		this.isFirstElement(newNode);
-		
+
 		newNode.next = head;
 		head = newNode;
 		elements++;
 	}
-	
-	public void insertAt(Node<T> newNode, int index) {
-		if(index < 0 || index > elements) {
+
+	public void insertAt(T element, int index) {
+		if (index < 0 || index > elements) {
+			throw new IndexOutOfBoundsException("Index is out of bounds.");
+		}
+
+		if (index == 0) {
+			this.prepend(element);
+			return;
+		}
+
+		if (index == elements) {
+			this.append(element);
+			return;
+		}
+
+		Node<T> newNode = new Node<>(element);
+		Node<T> currentNode = head;
+		for (int i = 0; i < index; i++) {
+			currentNode = currentNode.next;
+		}
+
+		newNode.next = currentNode.next;
+		currentNode.next = newNode;
+		elements++;
+	}
+
+	public void removeAt(int index) {
+		if (index < 0 || index > elements) {
 			throw new IndexOutOfBoundsException("Index is out of bounds.");
 		}
 		
-		if(index == 0) {
-			this.prepend(newNode);
-			return;
+		if (index == 0) {
+
+			head = head.next;
+		} else {
+			Node<T> currentNode = head;
+			for (int i = 0; i < index; i++) {
+				currentNode = currentNode.next;
+			}
+			currentNode.next = currentNode.next.next;
 		}
-		
-		if(index == elements) {
-			this.append(newNode);
-			return;
-		}
-		
-		Node<T> currentNode = head;
-		for(int i = 0; i < index; i++) {
-			currentNode = currentNode.next;
-		}
-		
-		newNode.next = currentNode.next;
-		currentNode.next = newNode;		elements++;
+		elements--;
 	}
-	
-	public void removeAt(Node<T> node, int index) {
-		Node<T> currentNode = head;
-		for(int i = 0; i < index; i++) {
-			currentNode = currentNode.next;
-		}
-	}
-	
+
 	public void removeLast() {
 		Node<T> currentNode = head;
-		while(!currentNode.next.equals(tail)) {
+		while (!currentNode.next.equals(tail)) {
 			currentNode = currentNode.next;
 		}
 		currentNode.next = null;
 		tail = currentNode;
 		elements--;
 	}
-	
+
 	public void removeFirst() {
-		if(elements == 0) return;
-		
+		if (elements == 0)
+			return;
+
 		head = head.next;
 		elements--;
 	}
@@ -125,7 +144,7 @@ public class LinkedList <T> {
 	public void printList() {
 		Node<T> currentNode = head;
 		StringBuilder sb = new StringBuilder();
-		while(currentNode != null) {
+		while (currentNode != null) {
 			sb.append(currentNode.value);
 			sb.append(" -> ");
 			currentNode = currentNode.next;
@@ -134,5 +153,5 @@ public class LinkedList <T> {
 		String str = sb.toString();
 		System.out.println(str);
 	}
-	
+
 }
--- src/datastructures/queue/Queue.java
+++ src/datastructures/queue/Queue.java
@@ -1,5 +1,41 @@
 package datastructures.queue;
 
-public class Queue {
-
+public class Queue <T> {
+	private Node<T> first;
+	private Node<T> last;
+	private int length;
+	
+	public Queue() {
+		this.first = null;
+		this.last = null;
+		this.length = 0;
+	}
+	
+	public Queue(T element) {
+		Node<T> newNode = new Node<>(element);
+		this.first = newNode;
+		this.last = newNode;
+		this.length = 1;
+	}
+	
+	public void enqueue(T element) {
+		Node<T> newNode = new Node<>(element);
+		if(length == 0) {
+			first = newNode;
+			last = newNode;
+		} else {
+			last.next = newNode;
+			last = newNode;
+		}
+		length++;
+	}
+	
+	public Node<T> dequeue() {
+		if(length == 0) return null;
+		Node<T> node = first;
+		if(length == 1) {
+			first = null;
+			last = null;
+		}
+	}
 }
